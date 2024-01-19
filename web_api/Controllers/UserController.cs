1d0f2acad5a83e959fa33d2008b573b6802e8bfd
mdtthanh <maithanh0131@gmail.com>
2024-01-19 10:03:45 +0700

    api crud

--- web_api/Controllers/UserController.cs
+++ web_api/Controllers/UserController.cs
@@ -32,6 +32,46 @@ namespace web_api.Controllers
             var employees = await _context.Employee.Include(x => x.WorkPlace).ToListAsync();
             return Ok(employees);
         }
+        //api add new all info off employee end workplace
+        [HttpPost("AddAllInfo")]
+        public async Task<ActionResult<Employee>> AddAllInfo(Employee employee)
+        {
+            _context.Employee.Add(employee);
+            await _context.SaveChangesAsync();
+            return Ok(employee);
+        }
 
+        // api edit info of employee 
+        [HttpPut("EditInfo/{id}")]
+        public async Task<ActionResult<Employee>> EditInfo(int id, Employee employee)
+        {
+            var employeeEdit = await _context.Employee.FindAsync(id);
+            if (employeeEdit == null)
+            {
+                return NotFound();
+            }
+            employeeEdit.Username = employee.Username;
+            employeeEdit.Email = employee.Email;
+            employeeEdit.Password = employee.Password;
+            employeeEdit.Description = employee.Description;
+            employeeEdit.WorkTime = employee.WorkTime;
+            employeeEdit.WorkPlaceId = employee.WorkPlaceId;
+            await _context.SaveChangesAsync();
+            return Ok(employeeEdit);
+        }
+
+        // api delete info of employee
+        [HttpDelete("DeleteInfo/{id}")]
+        public async Task<ActionResult<Employee>> DeleteInfo(int id)
+        {
+            var employeeDelete = await _context.Employee.FindAsync(id);
+            if (employeeDelete == null)
+            {
+                return NotFound();
+            }
+            _context.Employee.Remove(employeeDelete);
+            await _context.SaveChangesAsync();
+            return Ok(employeeDelete);
+        }
     }
 }
--- web_api/Program.cs
+++ web_api/Program.cs
@@ -1,10 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using web_api.Data;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -12,8 +12,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-
 });
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +38,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Use CORS policy
+app.UseCors("AllowSpecificOrigin");
 
 app.MapControllers();
 
